b12c0ed8d8dce4d5642bf53f1b4c89646caac35f
riodelphino <ryo.heavenlysky@gmail.com>
2024-12-26 20:50:10 +0900

    Mod opts / Fix notify

--- lua/mql_compile.lua
+++ lua/mql_compile.lua
@@ -1,23 +1,20 @@
 local M = {}
 
-local fn = require('mql_compile.functions')
 local opt = require('mql_compile.options')
+local fn = require('mql_compile.functions')
 local cmd = require('mql_compile.commands')
 
 local _loaded = false
 
 
 function M.setup(opts)
-   if _loaded then
-      return
-   end
-
+   -- Check already loaded or not
+   if _loaded then return end
    _loaded = true
 
+   -- Initialize plugin
    opt._os_type = fn.get_os_type()
-
    opt._opts = opt.merge(opts)
-
    cmd.create_commands()
 end
 
